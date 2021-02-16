a6508b61dc7c05e531fae5c00ab42eeee47f5d6c
JasterRogue <ryanmadigan141@gmail.com>
2021-02-16 19:52:37 +0000

    - Added all project files
    Signed-off-by: JasterRogue <ryanmadigan141@gmail.com>

---
+++ Touch/Assets/Scripts/IControllable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IControllable
+{
+    void youveBeenTouched();
+
+    void moveTo(Vector3 destination);
+
+    void scale(float percentageChange);
+}
+
